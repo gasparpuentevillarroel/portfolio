f86d3de240f5a58b8c17855a7b10c37c1eb079f4
gasparpuentevillarroel <gasparpuentevillarroel2006@gmail.com>
2026-09-14 20:54:12 -0300

    del: se eliminaron los archivos de web en mantenimiento

--- mi-portfolio/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-// astro.config.mjs
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-
-export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
-});
